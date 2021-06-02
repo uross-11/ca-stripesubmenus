3bbbadc6f921fb24a2b0563ecef34465d33f69c1
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-06-02 15:08:04 +0200

    Submenu location change

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,13 @@ import { useGlobalContext } from './context'
 
 const Navbar = () => {
   const {openSidebar, openSubmenu, closeSubmenu} = useGlobalContext();
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent;
+    const tempBtn = e.target.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+    openSubmenu(page, { center, bottom });
+  }
   return <nav className="nav">
     <div className="nav-center">
       <div className="nav-header">
@@ -15,16 +22,16 @@ const Navbar = () => {
       </div>
       <ul className="nav-links">
         <li>
-          <button className="link-btn">products</button>
+          <button className="link-btn" onMouseOver={displaySubmenu}>products</button>
         </li>
         <li>
-          <button className="link-btn">developers</button>
+          <button className="link-btn" onMouseOver={displaySubmenu}>developers</button>
         </li>
         <li>
-          <button className="link-btn">company</button>
+          <button className="link-btn" onMouseOver={displaySubmenu}>company</button>
         </li>
       </ul>
-      <button className='btn signin-btn'>Sign in</button>
+      <button className='btn signin-btn' onMouseOver={displaySubmenu}>Sign in</button>
     </div>
   </nav>
 }
